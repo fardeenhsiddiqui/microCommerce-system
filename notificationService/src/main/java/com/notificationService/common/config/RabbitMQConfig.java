f95f8a89823dd09d83f2015c4b38a86a6942ebd3
fardeenhsiddiqui <fardeenhsiddiqui98@gmail.com>
2026-06-28 11:36:34 +0530

    NS: Made as client for discovery-server

--- notificationService/src/main/java/com/notificationService/common/config/RabbitMQConfig.java
+++ notificationService/src/main/java/com/notificationService/common/config/RabbitMQConfig.java
@@ -5,7 +5,7 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -52,7 +52,7 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter messageConverter() {
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
